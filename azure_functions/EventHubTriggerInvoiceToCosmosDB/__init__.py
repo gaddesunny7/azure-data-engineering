133d72670a5daa42695160d55bbd3609a928b56c
SUNNY NAGESH GADDE <gaddesunny7@gmail.com>
2022-04-03 23:28:14 +0200

    Add files via upload

---
+++ azure_functions/EventHubTriggerInvoiceToCosmosDB/__init__.py
@@ -0,0 +1,13 @@
+from typing import List
+import logging
+
+import azure.functions as func
+
+
+def main(events: List[func.EventHubEvent],
+        outputDocument: func.Out[func.Document]):
+
+    for event in events:
+        json_data = event.get_body()
+        outputDocument.set(func.Document.from_json(json_data))
+        logging.info('Event has been inserted into Cosmos Db')
